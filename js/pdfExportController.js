438df9a05045ffbc007c42c51d532bcd5d054f78
Ahmed Fathelbab <ahmedfathelbab@gmail.com>
2025-10-26 23:02:25 +0300

    trial to fix pdf button -11

--- js/pdfExportController.js
+++ js/pdfExportController.js
@@ -229,26 +229,6 @@ class ExportController {
             .attr("stroke", d => d3.rgb(color(d.index)).darker())
             .attr("d", arc);
 
-        group.append("text")
-            .each(d => (d.angle = (d.startAngle + d.endAngle) / 2))
-            .attr("dy", "0.35em")
-            .attr("transform", d => `
-                rotate(${(d.angle * 180 / Math.PI - 90)})
-                translate(${outerRadius + 5})
-                ${d.angle > Math.PI ? "rotate(180)" : ""}
-            `)
-            .attr("text-anchor", d => d.angle > Math.PI ? "end" : null)
-            .text((d, i) => nodeNames[i]);
-
-        svg.append("g")
-            .attr("fill-opacity", 0.67)
-            .selectAll("path")
-            .data(chords)
-            .join("path")
-            .attr("d", ribbon)
-            .attr("fill", d => color(d.target.index))
-            .attr("stroke", d => d3.rgb(color(d.target.index)).darker());
-
         const svgNode = svg.node();
         
         // Temporarily append to the body to ensure styles are applied
