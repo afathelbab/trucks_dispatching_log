ea7a6364365163ea0ab5d9e97b3a8a3fb2a88d50
Ahmed Fathelbab <ahmedfathelbab@gmail.com>
2025-10-27 00:18:28 +0300

    back to vscode-4

--- js/eventBus.js
+++ js/eventBus.js
@@ -5,6 +5,7 @@ class EventBus {
     }
 
     on(event, callback) {
+        console.log(`EventBus: listener added for event: ${event}`);
         if (!this.events[event]) {
             this.events[event] = [];
         }
@@ -12,11 +13,13 @@ class EventBus {
     }
 
     off(event, callback) {
+        console.log(`EventBus: listener removed for event: ${event}`);
         if (!this.events[event]) return;
         this.events[event] = this.events[event].filter(cb => cb !== callback);
     }
 
     emit(event, data) {
+        console.log(`EventBus: emitting event: ${event}`, data);
         if (!this.events[event]) return;
         this.events[event].forEach(callback => callback(data));
     }
